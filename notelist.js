e5a6686167c5a56311fea352ccebfc3c4fb67ee1
Phil Kim <79614110+maisonkodama@users.noreply.github.com>
2021-05-25 00:45:21 -0400

    got rid of table now notes are cards in a grid

--- notelist.js
+++ notelist.js
@@ -2,21 +2,24 @@ window.addEventListener("load", () => {
     let notesList = JSON.parse(localStorage.getItem("allNotes"));
     let noteBody = document.getElementById("notesContainer");
     let gridContent = document.getElementById("allNoteGrid");
+    let cardContent = document.getElementById("noteCards");
 
     if (notesList === null) {
-        noteBody.innerHTML = "<p class='lead'>There are no notes. <a href='/index.html' class='text-decoration-none'>Click here to create one.</a></p>"
+        cardContent.innerHTML = "<p class='lead'>There are no notes. <a href='/index.html' class='text-decoration-none'>Click here to create one.</a></p>"
     } else {
         document.getElementById("deleteAll").classList.remove("d-none"); //displays button to delete notes
         notesList.forEach(element => {
-        const noteIndex = notesList.indexOf(element);
-        if (element.title == "") {
-            gridContent.innerHTML += "<tr><td>" + noteIndex + "</td><td>*untitled note*</td><td>" + 
-        element.content + "</td><td>" + element.created_at + "</td></tr>";
-        } else {
-        gridContent.innerHTML += "<tr><td>" + noteIndex + "</td><td>" + element.title + "</td><td>" + 
-        element.content + "</td><td>" + element.created_at + "</td></tr>";
-        };
-    })
+            const noteIndex = notesList.indexOf(element);
+            if (element.title == "") {
+                cardContent.innerHTML += "<div class='col-md-3 h-75 maxh-500 of-scroll'><div class='p3'><h2 class='text-primary'>*untitled note*</h2>" 
+                + "<span class='form-text fsize-smol'>" + element.created_at + "</span></div>"
+                + "<div><p class='pt-1 pb-1'>" + element.content + "</p></div></div>";
+            } else {
+                cardContent.innerHTML += "<div class='col-md-3 h-75 maxh-500 of-scroll'><div class='p3'><h2 class='text-primary'>" + element.title + "</h2>"
+                + "<span class='form-text fsize-smol'>" + element.created_at + "</span></div>"
+                + "<div><p class='pt-1 pb-1'>" + element.content + "</p></div></div>";
+            };
+        });
 };
 });
 
