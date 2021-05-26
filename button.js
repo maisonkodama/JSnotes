6284480eecc132a49a40649693c9a5d7d641fe62
Phil Kim <79614110+maisonkodama@users.noreply.github.com>
2021-05-26 01:47:24 -0400

    for some reason the second note doesnt see the ID of the first note so it also becomes one but should be easy to debug when its not 2am and I have to work in a few hours

--- button.js
+++ button.js
@@ -1,49 +1,57 @@
 //button to save note
 document.getElementById("saveNote").addEventListener("click", () => {
-    
+
     if (document.getElementById("noteContent").value === "") {
         alert("Oops. It looks like the note is empty. Add some text before you can save.");
     } else {
 
-    // declares note values
-    let titleValue = document.getElementById("titleText").value;
-    let contentValue = document.getElementById("noteContent").value;
+        // declares note values
+        let titleValue = document.getElementById("titleText").value;
+        let contentValue = document.getElementById("noteContent").value;
 
-    // gets list of notes parsed into JSON, if no list exists, create empty array
-    let allMyNotes = JSON.parse(localStorage.getItem("allNotes"));
-    if (allMyNotes === null) allMyNotes = [];
+        // gets list of notes parsed into JSON, if no list exists, create empty array
+        let allMyNotes = JSON.parse(localStorage.getItem("allNotes"));
+        if (allMyNotes === null) allMyNotes = [];
 
-    // saves form as object. using value does not provide any data validation
-    const savedNote = {
-        title: titleValue,
-        content: contentValue,
-        created_at: Date().toString()
+        // saves form as object. using value does not provide any data validation
+        const savedNote = {
+            id: 1,
+            title: titleValue,
+            content: contentValue,
+            created_at: Date().toString(),
+        };
+
+        if (allMyNotes && allMyNotes.length > 1) {
+                let lastNoteID = allMyNotes[allMyNotes.length - 1].id;
+                console.log(lastNoteID);
+                let thisNoteID = lastNoteID + 1;
+                savedNote.id = thisNoteID
+        };
+
+        // prints values to console to check if object is initiated with expected values
+        console.log(savedNote);
+        console.log("note content " + savedNote.content);
+
+        // sets object in local storage
+        localStorage.setItem("pkNote", JSON.stringify(savedNote));
+        allMyNotes.push(savedNote);
+        localStorage.setItem("allNotes", JSON.stringify(allMyNotes));
+
+        document.getElementById("noteForm").reset(); // resets title after saving
+        document.getElementById("noteContent").value = ""; // resets textarea after saving
+
+        // layer of feedback on save button to show the note has been saved, will replace the text in the following block
+        const save_button = document.getElementById("saveNote");
+        save_button.classList.remove("btn-primary");
+        save_button.classList.add("btn-success");
+        save_button.innerHTML = "Saved";
+
+        setTimeout(() => {
+            save_button.classList.remove("btn-success");
+            save_button.classList.add("btn-primary");
+            save_button.innerHTML = "Save";
+        }, 1500);
     };
-
-    // prints values to console to check if object is initiated with expected values
-    console.log(savedNote);
-    console.log("note content " + savedNote.content);
-
-    // sets object in local storage
-    localStorage.setItem("pkNote", JSON.stringify(savedNote));
-    allMyNotes.push(savedNote);
-    localStorage.setItem("allNotes", JSON.stringify(allMyNotes));
-
-    document.getElementById("noteForm").reset(); // resets title after saving
-    document.getElementById("noteContent").value = ""; // resets textarea after saving
-
-    // layer of feedback on save button to show the note has been saved, will replace the text in the following block
-    const save_button = document.getElementById("saveNote");
-    save_button.classList.remove("btn-primary");
-    save_button.classList.add("btn-success");
-    save_button.innerHTML = "Saved";
-
-    setTimeout(() => {
-        save_button.classList.remove("btn-success");
-        save_button.classList.add("btn-primary");
-        save_button.innerHTML = "Save";
-         }, 1500);
-};
 });
 
 
