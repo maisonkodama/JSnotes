706cdd4e94f53e11aabfffb6d7cd53a43cf3ab72
Phil Kim <79614110+maisonkodama@users.noreply.github.com>
2021-06-03 20:53:33 -0400

    note title will now render in edit page along with the original title as a placeholder save changes button will print the title and content to console

--- edit.js
+++ edit.js
@@ -6,7 +6,10 @@ window.addEventListener("load", () => {
    let titleField = document.getElementById("editTitleText");
    let contentField = document.getElementById("editNoteContent");
 
-   if (noteToEdit.title != null) titleField.setAttribute("placeholder", noteToEdit.title); // sets title value to note title value if there is one
+   if (noteToEdit.title != null) {
+       titleField.value = noteToEdit.title;
+       titleField.setAttribute("placeholder", noteToEdit.title)
+    }; // sets title value to note title value if there is one
    contentField.value = noteToEdit.content; // sets note content value in textfield
 });
 
@@ -15,3 +18,10 @@ abandonBtn.addEventListener("click", () => {
     localStorage.removeItem("editIndex");
     window.location.replace("/notes.html")
 });
+
+let saveEditBtn = document.getElementById("editNoteBtn");
+saveEditBtn.addEventListener("click", () => {
+    let titleValue = document.getElementById("editTitleText").value;
+    let contentValue = document.getElementById("editNoteContent").value;
+    console.log(`title value: ${titleValue}, ` + `content value: ${contentValue}`)
+})
