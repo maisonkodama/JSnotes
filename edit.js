fb56918295e33f3ff46cb9ed467a5626b814c375
Phil Kim <79614110+maisonkodama@users.noreply.github.com>
2021-06-04 13:31:01 -0400

    cleanup of comments from last commit

--- edit.js
+++ edit.js
@@ -23,7 +23,6 @@ let saveEditBtn = document.getElementById("editNoteBtn");
 saveEditBtn.addEventListener("click", () => {
     let titleValue = document.getElementById("editTitleText").value;
     let contentValue = document.getElementById("editNoteContent").value;
-    console.log(`title value: ${titleValue}, ` + `content value: ${contentValue}`) //prints editable values to console
     
     const indexToEdit = JSON.parse(localStorage.getItem("editIndex"));
     let allNotes = JSON.parse(localStorage.getItem("allNotes"));
@@ -32,9 +31,6 @@ saveEditBtn.addEventListener("click", () => {
         noteToEdit.title = titleValue;
         noteToEdit.content = contentValue;
         noteToEdit.updated_at = Date().toString();
-    
-    console.log(noteToEdit);
-    console.log(allNotes);
 
     localStorage.setItem("allNotes", JSON.stringify(allNotes));
 
